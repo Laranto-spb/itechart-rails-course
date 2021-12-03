4e22dbe5fa21705cb7433c1bbf1cbd578b80c8ae
Larisa Antokhina <laranto1985@gmail.com>
2021-12-03 14:36:43 +0300

    Added people table related to User, customized User with name, generated User devise controller, show username when logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :validatable
+
+  has_many :people, dependent: :destroy
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  resources :people
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   root 'pages#home'
 end

c7a4ea948e6176c3333ccd23b5812929fbe28b74
Larisa Antokhina <laranto1985@gmail.com>
2021-12-10 11:07:27 +0300

    Fix: delete people rspec passed

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -3,23 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'People', type: :request do
-  before do
-    let(:user) do
-      User.create(id: 1, name: 'Larisa', email: 'lar@mail.ru', password: '123456', password_confirmation: '123456')
-    end
-    let(:person) { Person.create(name: 'Person1', user_id: 1) }
-  end
-
-  # tests fail
-
   describe 'CRUD with people' do
-    it 'creates a new person' do
-      expect(person).to be_valid
+    let(:user) do
+      User.create(id: 1, name: 'User', email: 'user@mail.ru', password: '123456', password_confirmation: '123456')
+    end
+
+    let(:person1) { Person.create(name: 'Person1', user_id: user.id) }
+    let(:person2) { Person.create(name: 'Person2', user_id: user.id) }
+
+    it 'creates 2 persons' do
+      expect(person1).to be_valid
     end
 
     it 'delete person' do
-      person.destroy
-      expect(Person.count).to eq(0)
+      person1.destroy
+      expect(Person.exists?(person1.id)).to be_falsey
     end
   end
 end

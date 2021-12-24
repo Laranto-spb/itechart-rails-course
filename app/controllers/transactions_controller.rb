47472df29f947552a7d5e09fe5a51624c829e633
Larisa Antokhina <laranto1985@gmail.com>
2021-12-24 12:40:32 +0300

    Added update actions for transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,7 @@
 class TransactionsController < ApplicationController
   before_action :set_transaction, only: %i[ show edit update destroy ]
+  before_action :set_person_category, only: [:destroy]
+  before_action :set_person_created_for, only: [:create, :update]
 
   # GET /transactions or /transactions.json
   def index
@@ -11,6 +13,7 @@ class TransactionsController < ApplicationController
 
   # GET /transactions/new
   def new
+    @people = current_user.people
     @transaction = Transaction.new
   end
 
@@ -24,7 +27,7 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.save
-        format.html { redirect_to @transaction, notice: "Transaction was successfully created." }
+        format.html { redirect_to person_path(@person), notice: "Transaction was successfully created." }
         format.json { render :show, status: :created, location: @transaction }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -37,7 +40,7 @@ class TransactionsController < ApplicationController
   def update
     respond_to do |format|
       if @transaction.update(transaction_params)
-        format.html { redirect_to @transaction, notice: "Transaction was successfully updated." }
+        format.html { redirect_to person_path(@person), notice: "Transaction was successfully updated." }
         format.json { render :show, status: :ok, location: @transaction }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -48,7 +51,6 @@ class TransactionsController < ApplicationController
 
   # DELETE /transactions/1 or /transactions/1.json
   def destroy
-    @person = current_user.people.find(@transaction.person_category.person_id)
     @transaction.destroy
     respond_to do |format|
       format.html { redirect_to person_path(@person), notice: "Transaction was successfully destroyed." }
@@ -65,5 +67,13 @@ class TransactionsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def transaction_params
       params.require(:transaction).permit(:amount_value, :person_category_id)
+    end
+
+    def set_person_category
+      @person = current_user.people.find(@transaction.person_category.person_id)
+    end
+
+    def set_person_created_for
+      @person =  Person.find(PersonCategory.find(transaction_params[:person_category_id]).person_id)
     end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :person_category
+  validates :amount_value, presence: true
 end

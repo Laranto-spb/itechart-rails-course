83ece19a0c07ff94b7ab52a408d3ffa605ca1c61
Larisa Antokhina <laranto1985@gmail.com>
2021-12-24 15:36:49 +0300

    Added rspecs for Transaction, rubocoped, some view fix

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: %i[ show edit update destroy ]
+  before_action :set_transaction, only: %i[show edit update destroy]
   before_action :set_person_category, only: [:destroy]
-  before_action :set_person_created_for, only: [:create, :update]
+  before_action :set_person_created_for, only: %i[create update]
 
   # GET /transactions or /transactions.json
-  def index
-  end
+  def index; end
 
   # GET /transactions/1 or /transactions/1.json
-  def show
-  end
+  def show; end
 
   # GET /transactions/new
   def new
@@ -18,8 +18,7 @@ class TransactionsController < ApplicationController
   end
 
   # GET /transactions/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /transactions or /transactions.json
   def create
@@ -27,7 +26,7 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.save
-        format.html { redirect_to person_path(@person), notice: "Transaction was successfully created." }
+        format.html { redirect_to person_path(@person), notice: 'Transaction was successfully created.' }
         format.json { render :show, status: :created, location: @transaction }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +39,7 @@ class TransactionsController < ApplicationController
   def update
     respond_to do |format|
       if @transaction.update(transaction_params)
-        format.html { redirect_to person_path(@person), notice: "Transaction was successfully updated." }
+        format.html { redirect_to person_path(@person), notice: 'Transaction was successfully updated.' }
         format.json { render :show, status: :ok, location: @transaction }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,27 +52,28 @@ class TransactionsController < ApplicationController
   def destroy
     @transaction.destroy
     respond_to do |format|
-      format.html { redirect_to person_path(@person), notice: "Transaction was successfully destroyed." }
+      format.html { redirect_to person_path(@person), notice: 'Transaction was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_transaction
-      @transaction = Transaction.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def transaction_params
-      params.require(:transaction).permit(:amount_value, :person_category_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
 
-    def set_person_category
-      @person = current_user.people.find(@transaction.person_category.person_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def transaction_params
+    params.require(:transaction).permit(:amount_value, :person_category_id)
+  end
 
-    def set_person_created_for
-      @person =  Person.find(PersonCategory.find(transaction_params[:person_category_id]).person_id)
-    end
+  def set_person_category
+    @person = current_user.people.find(@transaction.person_category.person_id)
+  end
+
+  def set_person_created_for
+    @person = Person.find(PersonCategory.find(transaction_params[:person_category_id]).person_id)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Transaction < ApplicationRecord
   belongs_to :person_category
   validates :amount_value, presence: true
+  validates :amount_value, numericality: { greater_than: 0.0 }
 end

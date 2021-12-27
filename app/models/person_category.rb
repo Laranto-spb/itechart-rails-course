77745c048398c07c22b3f33293b449c638865343
Larisa Antokhina <laranto1985@gmail.com>
2021-12-27 09:48:38 +0300

    Correct new transaction form - list of person-catgory

--- app/models/person_category.rb
+++ app/models/person_category.rb
@@ -4,4 +4,8 @@ class PersonCategory < ApplicationRecord
   belongs_to :person
   belongs_to :category
   has_many :transactions, dependent: :destroy
+
+  def select_title
+    "Person: #{person.name}, Category: #{category.name} (#{category.transaction_type})"
+  end
 end

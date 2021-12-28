576ebea6731d7c38a1d81973a1797b100918f48d
Larisa Antokhina <laranto1985@gmail.com>
2021-12-28 12:03:43 +0300

    Added rspec for Note

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,5 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    let!(:note) { Note.create(body: 'Some note') }
+
+    it 'is not empty' do
+      note.body = nil
+      expect(note).not_to be_valid
+    end
+
+    it 'does not have too long name' do
+      note.body = 'LongLong' * 10
+      expect(note).not_to be_valid
+    end
+
+    it 'does not contain only whitespaces' do
+      note.body = '     '
+      expect(note).not_to be_valid
+    end
+  end
+
+  describe 'CRUD actions' do
+    let!(:user) do
+      User.create(name: 'User', email: 'user@mail.ru', password: '123456', password_confirmation: '123456')
+    end
+    let!(:person) { Person.create(name: 'Person', user_id: user.id) }
+
+    it 'creates new note' do
+      expect { Note.create(body: 'Some description') }.to change(Note, :count).by(1)
+    end
+
+    it 'deletes note' do
+      note = Note.create(body: 'Some description')
+      expect { note.destroy }.to change(Note, :count).by(-1)
+    end
+
+    it 'deletes with transaction' do
+      person.categories << Category.create(name: 'Category')
+      transaction = MoneyTransaction.create(amount_value: 2000, person_category_id: person.person_categories.first.id)
+      note = Note.create(body: 'Some description')
+      transaction.note = note
+      expect { transaction.destroy }.to change(Note, :count).by(-1)
+    end
+  end
 end
--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe '/notes', type: :request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Valid pages' do
+    let!(:user) do
+      User.create(name: 'Larisa', email: 'lar@mail.ru', password: '123456', password_confirmation: '123456')
+    end
+    let!(:person) { Person.create(name: 'Person', user_id: user.id) }
+
+    before do
+      login_as(user, scope: :user)
+    end
+
+    it 'has not general list of notes' do
+      expect { get '/notes' }.to raise_error(ActionController::RoutingError)
+    end
+  end
 end

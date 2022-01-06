34e3422831ab3c7ada290097c86910126925b081
Larisa Antokhina <laranto1985@gmail.com>
2022-01-06 13:52:52 +0300

    Corrected rspecs for Notes and MoneyTransaction

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -17,4 +17,28 @@ RSpec.describe '/notes', type: :request do
       expect { get '/notes' }.to raise_error(ActionController::RoutingError)
     end
   end
+
+  describe 'CRUD actions' do
+    let!(:user) do
+      User.create(name: 'User', email: 'user@mail.ru', password: '123456', password_confirmation: '123456')
+    end
+    let!(:person) { Person.create(name: 'Person', user_id: user.id) }
+
+    it 'creates new note' do
+      expect { Note.create(body: 'Some description') }.to change(Note, :count).by(1)
+    end
+
+    it 'deletes note' do
+      note = Note.create(body: 'Some description')
+      expect { note.destroy }.to change(Note, :count).by(-1)
+    end
+
+    it 'deletes with transaction' do
+      person.categories << Category.create(name: 'Category')
+      transaction = MoneyTransaction.create(amount_value: 2000, person_category_id: person.person_categories.first.id)
+      note = Note.create(body: 'Some description')
+      transaction.note = note
+      expect { transaction.destroy }.to change(Note, :count).by(-1)
+    end
+  end
 end
